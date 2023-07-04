cfc43ddd3d614a028e0eb794e9bd400d62d1ac6b
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-07-04 15:55:04 +0100

    Add first tests

---
+++ tests/test_import.py
@@ -0,0 +1,12 @@
+def test_import():
+    import tempor.clinic
+
+    version = tempor.clinic.__version__
+    assert version != ""
+
+
+def test_version():
+    import tempor.clinic
+
+    version = tempor.clinic.__version__
+    assert version != ""
